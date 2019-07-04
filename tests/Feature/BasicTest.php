4893708b40bad6da7fc31de0a7976aa7380b6bfa
Andrey Vusaty <andrey.vusatiy@softheme.com>
2019-07-04 19:58:02 +0300

    there is no devtools.sh yet; also the command 'compose normalize' failed

---
+++ tests/Feature/BasicTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ArieTimmerman\Laravel\SCIMServer\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
+
+/*
+use Orchestra\Testbench\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BasicTest extends TestCase {
+    
+    protected $baseUrl = 'http://localhost';
+    
+    use RefreshDatabase;
+    
+    
+    
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->loadLaravelMigrations('testbench');
+        
+        $this->withFactories(realpath(dirname(__DIR__).'/database/factories'));
+        
+        \ArieTimmerman\Laravel\SCIMServer\RouteProvider::routes();;
+        
+        factory(\ArieTimmerman\Laravel\SCIMServer\Tests\Model\User::class, 100)->create();
+        
+    }
+
+    protected function getEnvironmentSetUp($app) {
+
+        
+        $app ['config']->set ( 'app.url','http://localhost');;
+                
+        // Setup default database to use sqlite :memory:
+        
+        $app['config']->set('scimserver.Users.class', \ArieTimmerman\Laravel\SCIMServer\Tests\Model\User::class);
+        
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+    }
+    
+    public function testGet() {
+        
+        $response = $this->get('/scim/v2/Users');
+       
+        $response->assertStatus(200);
+        
+    }
+
+*/
---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ArieTimmerman\Laravel\SCIMServer\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
